d41b3faf448593c1ce829d2e6ee624bf1b22541a
chimeddorj <chimeddorj@gmail.com>
2024-07-03 17:51:38 +0900

    fix: schema 추가

--- src/main/java/com/example/web10/response/ErrorResult.java
+++ src/main/java/com/example/web10/response/ErrorResult.java
@@ -1,13 +1,21 @@
 package com.example.web10.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ErrorResult{
+public class ErrorResult {
 
+    @Schema(
+            example = "404"
+    )
     private int status;
+
+    @Schema(
+            example = "Not Found"
+    )
     private String message;
 
     @Override
