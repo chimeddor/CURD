7d378eccd2062214c511f821e1b7b61817d9162c
chimeddorj <chimeddorj@gmail.com>
2024-07-03 17:42:33 +0900

    fix: schema 추가 fix: static ValidErrorResponse comment

--- src/main/java/com/example/web10/response/ValidErrorResponse.java
+++ src/main/java/com/example/web10/response/ValidErrorResponse.java
@@ -1,5 +1,6 @@
 package com.example.web10.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,14 @@ import java.util.stream.Collectors;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ValidErrorResponse {
+    @Schema(
+            example = "Invalid input value"
+    )
     private String message;
+
+    @Schema(
+            example = "400"
+    )
     private int status;
     private List<FieldError> errors;
 
@@ -26,10 +34,10 @@ public class ValidErrorResponse {
         this.status = code.getStatus().value();
         this.errors = errors;
     }
-
-    public static ValidErrorResponse of(final ErrorCode code) {
-        return new ValidErrorResponse(code);
-    }
+//
+//    public static ValidErrorResponse of(final ErrorCode code) {
+//        return new ValidErrorResponse(code);
+//    }
 
     public static ValidErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
         return new ValidErrorResponse(code, FieldError.of(bindingResult));
@@ -38,7 +46,14 @@ public class ValidErrorResponse {
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class FieldError {
+        @Schema(
+                example = "uname"
+        )
         private String field;
+
+        @Schema(
+                example = "a"
+        )
         private String value;
 
         private FieldError(final String field, final String value) {
