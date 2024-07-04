8b303407c7c97f9b16cee2bdb8a52501545b3df4
chimeddorj <chimeddorj@gmail.com>
2024-07-04 17:42:51 +0900

    feat: valid 추가

--- src/main/java/com/example/web10/project/vo/RequestProjectIdVo.java
+++ src/main/java/com/example/web10/project/vo/RequestProjectIdVo.java
@@ -3,18 +3,21 @@ package com.example.web10.project.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class RequestProjectIdVo {
+
     @Schema(
             description = "project id",
             example = "1",
             type = "integer",
             nullable = false
     )
+    @NotNull
     @Min(1)
     private int pid;
 }
