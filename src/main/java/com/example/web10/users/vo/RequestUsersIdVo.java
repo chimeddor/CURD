e9137b767d7f9acf271998b42f9318b5773b84cc
chimeddorj <chimeddorj@gmail.com>
2024-07-04 17:50:12 +0900

    fix: schema를 수정 schema에 있는 miniLength을 지우고, type을 추가 feat: valid 추가 NotNull valid을 추가함

--- src/main/java/com/example/web10/users/vo/RequestUsersIdVo.java
+++ src/main/java/com/example/web10/users/vo/RequestUsersIdVo.java
@@ -2,6 +2,7 @@ package com.example.web10.users.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,9 +12,10 @@ public class RequestUsersIdVo {
     @Schema(
             description = "id",
             example = "1",
-            minLength = 1,
+            type = "integer",
             nullable = false
     )
+    @NotNull
     @Min(1)
     private int idN;
 }
