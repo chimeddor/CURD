339943707828cfbac24205de326e805025de26ae
chimeddorj <chimeddorj@gmail.com>
2024-07-05 10:26:46 +0900

    docs: Operation에 대한 설명 추가

--- src/main/java/com/example/web10/project/controller/ProjectController.java
+++ src/main/java/com/example/web10/project/controller/ProjectController.java
@@ -23,10 +23,14 @@ import java.util.List;
 @RequestMapping(value = "/project", method = RequestMethod.POST)
 public class ProjectController {
 
+    // reponseCode은 swagge에서 Code 부분
+    // description은 swagge에서 바로 보이는 description 부분
+    // swagge에서 바로 보이는 implementation에 있는 class가 있어야 처리 status과 메시지가 제대로 보인다. 부분
+
     @Operation(summary = "추가", description = "사용자의 프로젝트 추가",
             responses = {
                     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = Response.class))),
-                    @ApiResponse(responseCode = "400", description = "Invalid input value", content = @Content(schema = @Schema(implementation = ValidErrorResponse.class)))})
+                    @ApiResponse(responseCode = "400", description = "Invalid input value", content = @Content(schema = @Schema(implementation = NotFoundException.class)))})
     @RequestMapping("/insert")
     public @ResponseBody Response<?> insert(@Valid @RequestBody RequestProjectVo vo) throws Exception{
 
